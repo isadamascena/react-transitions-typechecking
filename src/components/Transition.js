1f1e4eb74673367aacaef09003ffdc91e02cab05
isadamascena <idamasce@thoughtworks.com>
2018-12-23 18:15:07 -0300

    more on transitions

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -16,20 +16,25 @@ class TransitionComp extends Component{
             <div>
                 <Transition
                     in={this.state.show}
-                    timeout={2000}
+                    timeout={{
+                        enter: 2000,
+                        exit: 2000
+                    }}
+                    enter={true}
+                    exit={true}
+                    
+                    onEnter={(node) => {
+                        console.log('Enter');
+                    }}
 
-                    mountOnEnter
-                    unmountOnExit
+                    onExit={(node) => {
+                        console.log('Exit');
+                    }}
                 >
                     {
                         state => 
-                        <div style={{
-                            background: 'red',
-                            height: '100px',
-                            transition: 'all 2s ease',
-                            opacity: state === 'exited' || state === 'exiting' ? 0 : 1
-                        }}>
-                            {state}
+                        <div className={`square square-${state}`}>
+                            {`square square-${state}`}
                         </div>
                     }
                 </Transition>
